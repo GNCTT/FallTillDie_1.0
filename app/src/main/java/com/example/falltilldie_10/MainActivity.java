107e1da023b6f9d25c6cbf4765bc9b89a8c2a0e5
ducanh <ducanh20176919@gmail.com>
2022-10-31 23:57:56 +0700

    tạo menu, thêm 1 số activity (chưa làm gì cả).

--- app/src/main/java/com/example/falltilldie_10/MainActivity.java
+++ app/src/main/java/com/example/falltilldie_10/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.falltilldie_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.example.falltilldie_10.HightCore.HightCore;
+import com.example.falltilldie_10.Setting.Setting;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +22,34 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickPlay(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickPlay2P(View view) {
+        Log.d("click button","play 2P");
+
+    }
+
+    public void clickExit(View view) {
+        finish();
+        System.exit(0);
+    }
+
+    public void clickContinue(View view) {
+        Log.d("click button", "continue");
+    }
+
+    public void clickHightCore(View view) {
+        Intent intent = new Intent(this, HightCore.class);
+        startActivity(intent);
+    }
+
+    public void clickSetting(View view) {
+        Intent intent = new Intent(this, Setting.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/falltilldie_10/Setting/Setting.java
@@ -0,0 +1,16 @@
+package com.example.falltilldie_10.Setting;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.falltilldie_10.R;
+
+public class Setting extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+    }
+}
